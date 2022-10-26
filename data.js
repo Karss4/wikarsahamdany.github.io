bce842b1e2ca84fb789d936452d94d84e8b58e56
wikarsahamdany <wikarsahamdany99@gmail.com>
2022-10-27 02:55:36 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,54 @@
+function bg1() {
+    let bg1 = document.getElementById("bgm1")
+
+bg1.addEventListener('mouseover', () => {
+    bgm1.style.backgroundImage = `url('Images/bg-gif1.gif')`;
+});
+
+}
+
+function bgb1() {
+
+    let bg1 = document.getElementById("bgm1")
+
+    bg1.addEventListener('mouseover', () => {
+        bgm1.style.backgroundImage = `url('Images/bg-11.gif')`;
+    });
+}
+
+function bg2() {
+    let bg1 = document.getElementById("bgm1")
+
+bg1.addEventListener('mouseover', () => {
+    bgm1.style.backgroundImage = `url('Images/letsgo-gif.gif')`;
+});
+
+
+
+}
+
+function bgb2() {
+
+    let bg1 = document.getElementById("bgm1")
+
+    bg1.addEventListener('mouseover', () => {
+        bgm1.style.backgroundImage = `url('Images/bg-11.gif')`;
+    });
+}
+
+let background = document.getElementById('background1')
+let mode = document.getElementById('mode')
+
+
+function darkMode() {
+    if (mode.textContent = "DarkMode" ) {
+        mode.textContent = `LightMode`;
+
+        background.classList.toggle("dark");
+        
+    }
+}
+
+
+[]
+
